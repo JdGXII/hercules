e04b9a00e095c775b0cb76fa4b3273c551026ed0
JdGXII <jralzaibar@yahoo.com>
2018-05-10 17:27:36 -0500

    Added services and empty js file

---
+++ Hercules/Services/CasoService.cs
@@ -0,0 +1,37 @@
+﻿using Hercules.DataAccess.DAOs;
+using Hercules.Factories;
+using Hercules.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hercules.Services
+{
+    public class CasoService
+    {
+        private DAOFactory _daoFactory;
+        private CasoDAO _casoDao;
+
+        public CasoService()
+        {
+            _daoFactory = new DAOFactory();
+            _casoDao = (CasoDAO)_daoFactory.GetCasoDAO();
+        }
+
+        public int AddCaso(Caso toBeCreated)
+        {
+            return _casoDao.Create(toBeCreated);
+        }
+
+        public int UpdateCaso(Caso toBeUpdated)
+        {
+            return _casoDao.Update(toBeUpdated);
+        }
+
+        public Caso GetCaso(int casoId)
+        {
+            return (Caso)_casoDao.RetrieveOne(casoId);
+        }
+    }
+}
--- Hercules/Services/UserService.cs
+++ Hercules/Services/UserService.cs
@@ -11,19 +11,32 @@ namespace Hercules.Services
     public class UserService
     {
         private DAOFactory _daoFactory;
+        private UserDAO _userDao;
 
         public UserService()
         {
             _daoFactory = new DAOFactory();
+            _userDao = (UserDAO)_daoFactory.GetUserDAO();
         } 
         
         public int AddUser(User newUser)
         {
-            UserDAO userDao = (UserDAO)_daoFactory.GetCasoDAO();
-
-            userDao.Create(newUser);
-            return 0;
+            
+            return _userDao.Create(newUser);
+            
         }
- 
+
+        public int UpdateUser(User toBeUpdate) {
+           
+
+            return _userDao.Update(toBeUpdate);
+        }
+
+        public User GetUser(int userId)
+        {
+            return (User)_userDao.RetrieveOne(userId);
+        }
+
+         
     }
 }
